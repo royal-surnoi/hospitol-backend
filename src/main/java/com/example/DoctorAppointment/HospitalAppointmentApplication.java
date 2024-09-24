e60b6195eeacb49174e8afe5bbda225507046d61
royal-surnoi <royalreddy@surnoi.com>
2024-09-24 11:14:35 +0530

    Update HospitalAppointmentApplication.java

--- src/main/java/com/example/DoctorAppointment/HospitalAppointmentApplication.java
+++ src/main/java/com/example/DoctorAppointment/HospitalAppointmentApplication.java
@@ -20,7 +20,7 @@ public class HospitalAppointmentApplication {
 			public void addCorsMappings(CorsRegistry registry) {
 				registry.addMapping("/**")
 				// .allowedOrigins("*")
-				.allowedOrigins("http://ec2-18-209-60-48.compute-1.amazonaws.com")
+				.allowedOrigins("http://ec2-52-23-160-113.compute-1.amazonaws.com")
 				.allowedMethods("GET", "POST", "PUT", "DELETE")
 				.allowedHeaders("*");
 
